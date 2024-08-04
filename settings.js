c1438af5b1dbeb072b30b555b409e6c1baf1e809
Bicomaru120 <bicomarudono@gmail.com>
2024-08-04 11:11:47 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,23 +3,23 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "923192173398"
-global.ownername = "🐛 MR WASI "
+global.ownernomer = "2349072642884"
+global.ownername = "🐛 EmmyGold "
 global.ytname = "YT: Dont have"
 global.socialm = "GitHub: private"
 global.location = "Pakistan, Lahore"
 
-global.ownernumber = '923347748038'  //creator number
-global.ownername = ' MR WASI ' //owner name
-global.botname = 'WASI TECH BUG BOT ' //name of the bot
+global.ownernumber = '2349072642884'  //creator number
+global.ownername = ' EmmyGold ' //owner name
+global.botname = 'Emmy TECH BUG BOT ' //name of the bot
 
 //sticker details
 global.packname = 'Sticker By'
-global.author = 'WASI'
+global.author = 'EmmyGold'
 
 //console view/theme
 global.themeemoji = '🪀'
-global.wm = "WASI TECH"
+global.wm = "Emmy TECH"
 
 //theme link
 global.link = 'https://whatsapp.com'
@@ -28,7 +28,7 @@ global.link = 'https://whatsapp.com'
 global.prefa = ['','!','.','#','&']
 
 //false=disable and true=enable
-global.autoRecording = false //auto recording
+global.autoRecording = true //auto recording
 global.autoTyping = false //auto typing
 global.autorecordtype = false //auto typing + recording
 global.autoread = false //auto read messages
